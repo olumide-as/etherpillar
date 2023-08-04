c6e75742adeb4d6214517d8b18363f18c5de74a7
olumide-as <adigunsamuel84@gmail.com>
2023-08-04 23:23:12 +0100

    Added twitter comment

--- src/Pages/Airdrop.jsx
+++ src/Pages/Airdrop.jsx
@@ -19,31 +19,27 @@ const Airdrop = () => {
         setStep(3);
     };
 
+    const handleClaimAirdrop = () => {
+        // Add your logic for handling the airdrop claim here
+        setStep(4);
+    };
+
     return (
-        <div className="flex min-h-screen flex-col justify-center items-center px-4 py-16 mx-auto md:px-24 lg:px-8 lg:py-10 bg-no-repeat bg-cover bg-center"  
-        style={{
-            backgroundImage: `url(${Test})`,
-          }}>
-            <div className="mb-10 sm:text-center md:mb-12" >
+        <div className="flex min-h-screen flex-col justify-center items-center px-4 py-16 mx-auto md:px-24 lg:px-8 lg:py-10 bg-no-repeat bg-cover bg-center"
+            style={{
+                backgroundImage: `url(${Test})`,
+            }}>
+            <div className="mb-10 sm:text-center md:mb-12">
 
 
                 <div className="container mx-auto p-4">
                     <div className="space-y-4">
 
-                        {/* Step 3 */}
-                        <div>
-                            <h2 className="mb-6 text-3xl font-bold leading-none tracking-tight md:text-6xl md:mx-auto">
-                                {step === 3 ? 'Congratulations!' : isConnected ? 'Please Claim Airdrop' : 'Please Log in and Continue'}
-                            </h2>
-                            {step === 3 && <p className="text-lg">You are eligible for the airdrop</p>}
-                            {step === 3 &&
-                                <button className="bg-white inline-flex items-center justify-center h-12 px-6 my-3 font-bold tracking-wide text-black hover:scale-105 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">
-                                    Share
-                                </button>}
-                        </div>
-
                         {/* Step 1 */}
                         <div className={step === 1 ? '' : 'hidden'}>
+                            <h2 className="mb-6 text-3xl font-bold leading-none tracking-tight md:text-6xl md:mx-auto">
+                                {isConnected ? 'Please Claim Airdrop' : 'Please Log in and Continue'}
+                            </h2>
                             <button
                                 className="bg-white inline-flex items-center justify-center h-12 px-6 font-bold tracking-wide text-black hover:scale-105 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                 disabled={isConnected}
@@ -55,7 +51,10 @@ const Airdrop = () => {
 
                         {/* Step 2 */}
                         <div className={step === 2 ? '' : 'hidden'}>
-                            <form onSubmit={handleSubmitTwitterId}>
+                            <h2 className="mb-6 text-3xl font-bold leading-none tracking-tight md:text-6xl md:mx-auto">
+                                Verify your Account
+                            </h2>
+                            <form>
                                 <div className="space-y-2 flex flex-col">
                                     <label htmlFor="Label" className='font-bold'>Input your Twitter ID</label>
                                     <input
@@ -64,7 +63,7 @@ const Airdrop = () => {
                                         id="twitterId"
                                         name="twitterId"
                                         placeholder='@example'
-                                        className="font-bold border border-gray-300 px-4 py-2 rounded focus:ring focus:ring-blue-200 focus:border-blue-500"
+                                        className="input-gray-500 font-bold text-black border border-gray-300 px-4 py-2 rounded focus:ring focus:ring-blue-200 focus:border-blue-500"
                                         value={twitterId}
                                         onChange={(e) => setTwitterId(e.target.value)}
                                     />
@@ -74,20 +73,63 @@ const Airdrop = () => {
                                     type="submit"
                                     className="bg-white inline-flex items-center justify-center h-12 my-3 px-6 font-bold tracking-wide text-black hover:scale-105 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                     disabled={!isConnected}
+                                    onClick={handleSubmitTwitterId}
                                 >
                                     Submit
                                 </button>
                             </form>
                         </div>
 
+                        {/* Step 3 */}
+                        <div className={step === 3 ? '' : 'hidden'}>
+                            <h2 className="mb-6 text-3xl font-bold leading-none tracking-tight md:text-6xl md:mx-auto">
+                                Congratulations!
+                            </h2>
+                            <p className="text-lg">You are eligible for the airdrop</p>
+                            <p className="py-4 text-lg">To Claim Airdrop, share, comment and tag 3 friends to claim</p>
+                            <button
+                                className="bg-white inline-flex items-center justify-center m-2 h-12 px-6 my-3 font-bold tracking-wide text-black hover:scale-105 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                                onClick={handleClaimAirdrop}
+                            >
+                                Share and @ 3 friends
+                            </button>
+                        </div>
 
+                        {/* Step 4 */}
+                        <div className={step === 4 ? '' : 'hidden'}>
+                            <h2 className="mb-6 text-3xl font-bold leading-none tracking-tight md:text-6xl md:mx-auto">
+                                Completed the Task?
+                            </h2>
+                            <p className="text-lg">Paste your comment link below</p>
+                            <form>
+                                <div className="space-y-2 flex flex-col">
+                                    <textarea
+                                        required
+                                        type="text"
+                                        id="twitterId"
+                                        name="twitterId"
+                                        placeholder='@example'
+                                        className="input-gray-500 font-bold text-black border border-gray-300 px-4 py-2 rounded focus:ring focus:ring-blue-200 focus:border-blue-500"
+                                        value={twitterId}
+                                        onChange={(e) => setTwitterId(e.target.value)}
+                                    />
 
+                                    
+
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="bg-white inline-flex items-center justify-center h-12 my-3 px-6 font-bold tracking-wide text-black hover:scale-105 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                                    disabled={!isConnected}
+                                    onClick={handleSubmitTwitterId}
+                                >
+                                    Claim Airdrop
+                                </button>
+                            </form>
+                        </div>
 
                     </div>
                 </div>
-
-
-
 
             </div>
         </div>
